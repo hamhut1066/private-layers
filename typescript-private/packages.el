746e94e262fdfbfa3cc3cd9474626ab384ac7b27
Hamish Hutchings <hamish@freeagent.com>
2015-07-28 13:46:35 +0100

    Add advice to remove flymake from being enabled.

--- typescript-private/packages.el
+++ typescript-private/packages.el
@@ -11,24 +11,32 @@
 ;;; License: GPLv3
 
 (setq typescript-private-packages '(
-                            tss
-                            flycheck))
+                                    tss
+                                    flycheck))
 
-;; (defun typescript/init-tss ()
-  ;; "Initialize my package"
-  ;; (use-package tss
-    ;; :defer t
-    ;; :mode ("\\.ts\\'" . typescript-mode)
-    ;; :init (evil-leader/set-key-for-mode 'typescript-mode
-            ;; "mgg" 'tss-jump-to-definition
-            ;; "mhh" 'tss-popup-help)
-    ;; :config (tss-config-default)))
-
+(setq test-layer-excluded-packages '(flymake))
 
 (defun typescript-private/post-init-flycheck ()
   "Add hook for flycheck"
-  "remove hook for flymake, as it seems to conflict."
-  (add-hook 'typescript-mode-hook 'flycheck-mode (flymake-mode nil)))
+  (use-package flycheck
+    :defer t
+    :config (progn
+              ;; ;;; remove hook for flymake, as it seems to conflict.
+              ;; (add-hook 'typescript-mode-hook 'flycheck-mode (flymake-mode nil))
+              ;; (add-hook 'typescript-mode-hook (lambda flymake-mode nil))
+              ;;; This Code Snippet was from [Here](https://github.com/caisah/flycheck-typescript) and is licensed under the GPL
+              (flycheck-define-checker typescript
+                "A TypeScript syntax checker using tsc command."
+                :command ("tsc" "--out" "/dev/null" source-inplace)
+                :error-patterns
+                ((error line-start (file-name) "(" line "," column "): error " (message) line-end))
+                :modes (typescript-mode))
+
+              (add-to-list 'flycheck-checkers 'typescript)
+              )
+    )
+  )
+
 
 (defun typescript-private/init-tss ()
   "Initialize tss"
@@ -38,28 +46,22 @@
            ("\\.ts\\'" . typescript-mode)
            ))
   :init (evil-leader/set-key-for-mode 'flycheck-mode
-                                      "mgg" 'tss-jump-to-definition
-                                      "mhh" 'tss-popup-help)
+          "mgg" 'tss-jump-to-definition
+          "mhh" 'tss-popup-help)
 
-  :config (progn
-            (require 'typescript)
-            (require 'tss)
-            (require 'flycheck)
-            (require 'evil)
-            (require 'evil-leader)
-            ;;; This Code Snippet was from [Here](https://github.com/caisah/flycheck-typescript) and is licensed under the GPL
-            (flycheck-define-checker typescript
-              "A TypeScript syntax checker using tsc command."
-              :command ("tsc" "--noImplicitAny --out" "/dev/null" source-inplace)
-              :error-patterns
-              ((error line-start (file-name) "(" line "," column "): error " (message) line-end))
-              :modes (typescript-mode))
-
-            (add-to-list 'flycheck-checkers 'typescript)
-            ;;; End of the snippet
-
-            (tss-config-default)
-
-
-            )
   )
+
+(defun typescript-private/post-init-tss ()
+  "Initialize tss"
+  (use-package tss
+    :defer t
+    :config (progn
+              (defun remove-flymake ()
+                "Removes Flymake mode after setting up the current buffer"
+                (flymake-mode nil)
+                )
+              (advice-add 'tss-setup-current-buffer :before #'remove-flymake)
+
+              (add-hook 'typescript-mode-hook 'tss-setup-current-buffer t)
+              (add-hook 'kill-buffer-hook 'tss--delete-process t)
+              )))
